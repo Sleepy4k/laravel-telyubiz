1057e39cbcfa1dac1d5f39fa9da6194065ea9f95
Apri Pandu W <sarahpalastrin@gmail.com>
2026-02-14 01:01:19 +0700

    feat(config): add new config and update auth config

---
+++ config/cors.php
@@ -0,0 +1,47 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Cross-Origin Resource Sharing (CORS) Configuration
+    |--------------------------------------------------------------------------
+    |
+    | Here you may configure your settings for cross-origin resource sharing
+    | or "CORS". This determines what cross-origin operations may execute
+    | in web browsers. You are free to adjust these settings as needed.
+    |
+    | To learn more: https://developer.mozilla.org/en-US/docs/Web/HTTP/CORS
+    |
+    */
+
+    'paths' => ["*." . parse_url(config('app.url'), PHP_URL_HOST), 'sanctum/csrf-cookie'],
+
+    'allowed_methods' => ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+
+    'allowed_origins' => config('app.env') === 'production'
+        ? ['https://your-production-domain.com']
+        : ['https://telyubiz.test'],
+
+    'allowed_origins_patterns' => [],
+
+    'allowed_headers' => [
+        'Content-Type',
+        'X-Requested-With',
+        'Authorization',
+        'Accept',
+        'Origin',
+        'Referer',
+        'User-Agent',
+    ],
+
+    'exposed_headers' => [
+        'Authorization',
+        'Content-Disposition',
+    ],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
