b077c70916561fb96ddeab11011194caa0df063d
Apri Pandu W <sarahpalastrin@gmail.com>
2026-02-14 00:58:27 +0700

    chore(route): update web and api route

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,34 @@
 <?php
 
-use Illuminate\Http\Request;
+use App\Http\Controllers\Api\Auth;
+use App\Http\Controllers\Api\OAuth;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::post('/auth/login', Auth\LoginController::class)->name('api.login');
+Route::post('/auth/register', Auth\RegisterController::class)->name('api.register');
+
+Route::prefix('/oauth')->name('api.oauth.')->group(function () {
+    Route::prefix('/google')->name('google.')->group(function () {
+        Route::get('/redirect', [OAuth\GoogleController::class, 'redirect'])->name('redirect');
+        Route::get('/callback', [OAuth\GoogleController::class, 'callback'])->name('callback');
+    });
+
+    Route::prefix('/microsoft')->name('microsoft.')->group(function () {
+        Route::get('/redirect', [OAuth\MicrosoftController::class, 'redirect'])->name('redirect');
+        Route::get('/callback', [OAuth\MicrosoftController::class, 'callback'])->name('callback');
+    });
+});
+
+Route::post('/forgot-password', [Auth\ForgotPasswordController::class, 'sendResetLink'])->name('api.forgot-password');
+Route::post('/reset-password', [Auth\ForgotPasswordController::class, 'resetPassword'])->name('api.reset-password');
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::delete('/auth/logout', Auth\LogoutController::class)->name('api.logout');
+
+    Route::middleware('verified.email')->prefix('/email')->name('verification.')->group(function () {
+        Route::controller(Auth\VerificationController::class)->group(function () {
+            Route::post('/verification-notification', 'resend')->name('send');
+            Route::get('/verify/{id}/{hash}', 'verify')->name('verify');
+        });
+    });
+});
