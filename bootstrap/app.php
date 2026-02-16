613e10068fcd012dc5705ca08b2a0d8fbdb1a82b
Apri Pandu W <sarahpalastrin@gmail.com>
2026-02-16 23:39:34 +0700

    chore(bootstrap): fix bootstrap file format

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -7,6 +7,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Response as FacadesResponse;
 use Illuminate\Support\Facades\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Throwable;
 
 return Application::configure(basePath: dirname(__DIR__))
     ->withRouting(
@@ -26,7 +27,7 @@ return Application::configure(basePath: dirname(__DIR__))
         $middleware->throttleApi();
         $middleware->web(
             prepend: [
-                \Illuminate\Routing\Middleware\ThrottleRequests::class . ':web',
+                \Illuminate\Routing\Middleware\ThrottleRequests::class.':web',
             ],
             append: [
                 \App\Http\Middleware\ImplementPageSpeed::class,
@@ -52,33 +53,26 @@ return Application::configure(basePath: dirname(__DIR__))
                 str_starts_with($currentDomain, 'www.') ||
                 $currentDomain === parse_url(config('app.url'), PHP_URL_HOST)
             ) {
-                return (new \App\Http\Middleware\ImplementPageSpeed())
-                    ->handle($request, fn($req) => $response);
+                return (new \App\Http\Middleware\ImplementPageSpeed)
+                    ->handle($request, fn ($req) => $response);
             }
 
             return match (true) {
                 $exception instanceof \Illuminate\Database\Eloquent\ModelNotFoundException,
-                $exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException =>
-                    FacadesResponse::error('Resource not found', null, 404),
+                $exception instanceof \Symfony\Component\HttpKernel\Exception\NotFoundHttpException => FacadesResponse::error('Resource not found', null, 404),
 
-                $exception instanceof \Illuminate\Validation\ValidationException =>
-                    FacadesResponse::error('The given data was invalid.', $exception->errors(), 422),
+                $exception instanceof \Illuminate\Validation\ValidationException => FacadesResponse::error('The given data was invalid.', $exception->errors(), 422),
 
-                $exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException =>
-                    FacadesResponse::error('Method Not Allowed', null, 405),
+                $exception instanceof \Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException => FacadesResponse::error('Method Not Allowed', null, 405),
 
-                $exception instanceof \Symfony\Component\HttpKernel\Exception\HttpException =>
-                    FacadesResponse::error($exception->getMessage(), null, $exception->getStatusCode()),
+                $exception instanceof \Symfony\Component\HttpKernel\Exception\HttpException => FacadesResponse::error($exception->getMessage(), null, $exception->getStatusCode()),
 
                 $exception instanceof \Illuminate\Auth\Access\AuthorizationException ||
-                $exception instanceof \Laravel\Sanctum\Exceptions\MissingAbilityException =>
-                    FacadesResponse::error('You do not have the required permissions to access this resource.', null, 403),
+                $exception instanceof \Laravel\Sanctum\Exceptions\MissingAbilityException => FacadesResponse::error('You do not have the required permissions to access this resource.', null, 403),
 
-                $exception instanceof \Illuminate\Auth\AuthenticationException =>
-                    FacadesResponse::error('You are not authenticated to access this resource.', null, 401),
+                $exception instanceof \Illuminate\Auth\AuthenticationException => FacadesResponse::error('You are not authenticated to access this resource.', null, 401),
 
-                default =>
-                    FacadesResponse::error('An unexpected error occurred. Please try again later.', null, 500),
+                default => FacadesResponse::error('An unexpected error occurred. Please try again later.', null, 500),
             };
         });
     })->create();
